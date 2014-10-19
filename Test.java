92f535b11a926fd4afbc1f98d1a3e1892a62ed7d
Poofplonker <michaelglenny@gmail.com>
2014-10-20 12:56:23 +1300

    Adapted SinCos example to run multiple times, generate py generates random file

--- Test.java
+++ Test.java
@@ -14,7 +14,11 @@
  * limitations under the License.
  */
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
+
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.events.LearningEvent;
 import org.neuroph.core.events.LearningEventListener;
@@ -34,71 +38,90 @@ import org.neuroph.util.TransferFunctionType;
  */
 public class Test implements LearningEventListener {
 
-    public static void main(String[] args) {
-        new Test().run();
-    }
-    
-    /**
-     * Runs this sample
-     */
-    public void run() {
-    	final int NODESINHIDDEN = 10;
-    	final int INPUTNODES = 1;
-    	final int OUTPUTNODES = 1;
-        // create training set (logical XOR function)
-        DataSet trainingSet = DataSet.createFromFile("SinCos100000.csv", 1,1, ",", false);
-        // create multi layer perceptron
-        MultiLayerPerceptron myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, INPUTNODES, NODESINHIDDEN, OUTPUTNODES);
+	BufferedWriter br;
+	public static void main(String[] args) throws IOException {
+		new Test().run(1,"SinCos1000.csv","SinCosRand1000.csv", "TestRunOutput.txt");
+	}
 
-        BackPropagation learningRule = new BackPropagation();
-        learningRule.addListener(this);
-        learningRule.setLearningRate(0.1);
-        learningRule.setMaxError(0.01);
-        learningRule.setMaxIterations(20000);
-        learningRule.setBatchMode(false);
-        myMlPerceptron.setLearningRule(learningRule);
-     
-        // learn the training set
-        System.out.println("Training neural network...");
-        myMlPerceptron.learn(trainingSet);
+	/**
+	 * Runs this sample
+	 * @throws IOException 
+	 */
+	public void run(int inputNodes, String filename, String randomFileName, String outputFile) throws IOException {
+		final int NODESINHIDDEN = 3;
+		final int INPUTNODES = 1;
+		final int OUTPUTNODES = 1;
+		br = new BufferedWriter(new FileWriter(outputFile));
+		for(int j = 0; j < 100; j++){
+			// create training set (logical XOR function)
+			DataSet trainingSet = DataSet.createFromFile(filename, 1,1, ",", false);
+			DataSet randomTrainingSet = DataSet.createFromFile(randomFileName, 2,1,",",false);
+			// create multi layer perceptron
+			MultiLayerPerceptron myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, INPUTNODES, NODESINHIDDEN, OUTPUTNODES);
+			BackPropagation learningRule = createLearningRule();
+			myMlPerceptron.setLearningRule(learningRule);
+			Double[] initialWeights = myMlPerceptron.getWeights();
 
-        // test perceptron
-        System.out.println("Testing trained neural network");
-        testNeuralNetwork(myMlPerceptron, trainingSet);
+			// learn the training set
+			System.out.println("Training neural network for first time...");
+			myMlPerceptron.learn(trainingSet);
 
-        // save trained neural network
-        myMlPerceptron.save("myMlPerceptron.nnet");
+			myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, INPUTNODES+1, NODESINHIDDEN, OUTPUTNODES);
+			myMlPerceptron.setLearningRule(learningRule);
+			int newWeightLength = initialWeights.length+NODESINHIDDEN;
+			double[] stupidArray = new double[newWeightLength];
+			for(int i = 0; i < newWeightLength; i++){
+				if(i < NODESINHIDDEN){
+					stupidArray[i] = initialWeights[i];
+				}
+				else if(i < 2*NODESINHIDDEN){
+					stupidArray[i] = 0;
+				}else{ 
+					stupidArray[i] = initialWeights[i-NODESINHIDDEN];
+				}
 
-        // load saved neural network
-        NeuralNetwork loadedMlPerceptron = NeuralNetwork.load("myMlPerceptron.nnet");
+			}
+			myMlPerceptron.setWeights(stupidArray);
+			myMlPerceptron.learn(randomTrainingSet);
+		}
 
-        // test loaded neural network
-        System.out.println("Testing loaded neural network");
-        testNeuralNetwork(loadedMlPerceptron, trainingSet);
-    }
+	}
+	public BackPropagation createLearningRule(){
+		BackPropagation learningRule = new BackPropagation();
+		learningRule.addListener(this);
+		learningRule.setLearningRate(0.1);
+		learningRule.setMaxError(0.001);
+		learningRule.setMaxIterations(20000);
+		learningRule.setBatchMode(false);
+		return learningRule;
+	}
+	/**
+	 * Prints network output for the each element from the specified training set.
+	 * @param neuralNet neural network
+	 * @param trainingSet training set
+	 */
+	public static void testNeuralNetwork(NeuralNetwork neuralNet, DataSet testSet) {
 
-    /**
-     * Prints network output for the each element from the specified training set.
-     * @param neuralNet neural network
-     * @param trainingSet training set
-     */
-    public static void testNeuralNetwork(NeuralNetwork neuralNet, DataSet testSet) {
+		for(DataSetRow testSetRow : testSet.getRows()) {
+			neuralNet.setInput(testSetRow.getInput());
+			neuralNet.calculate();
+			double[] networkOutput = neuralNet.getOutput();
 
-        for(DataSetRow testSetRow : testSet.getRows()) {
-            neuralNet.setInput(testSetRow.getInput());
-            neuralNet.calculate();
-            double[] networkOutput = neuralNet.getOutput();
+			System.out.print("Input: " + Arrays.toString( testSetRow.getInput() ) );
+			System.out.println(" Output: " + Arrays.toString( networkOutput) );
+		}
+	}
 
-            System.out.print("Input: " + Arrays.toString( testSetRow.getInput() ) );
-            System.out.println(" Output: " + Arrays.toString( networkOutput) );
-        }
-    }
-    
-    @Override
-    public void handleLearningEvent(LearningEvent event) {
-        BackPropagation bp = (BackPropagation)event.getSource();
-        if (event.getEventType() != LearningEventType.LEARNING_STOPPED)
-            System.out.println(bp.getCurrentIteration() + ". iteration : "+ bp.getTotalNetworkError());
-    }    
+	@Override
+	public void handleLearningEvent(LearningEvent event) {
+		BackPropagation bp = (BackPropagation)event.getSource();
+		if (event.getEventType() == LearningEventType.LEARNING_STOPPED)
+			try {
+				br.write(bp.getCurrentIteration() + ". iteration : "+ bp.getTotalNetworkError());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}    
 
 }
