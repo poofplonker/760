c99381e19db2ced939edd96fcb282e0e988b488e
Laurence McFarlane <laurenceium@gmail.com>
2014-10-20 23:04:01 +1300

    Added comments, cleaned constants and file names

--- Test.java
+++ Test.java
@@ -1,23 +1,6 @@
-/**
- * Copyright 2010 Neuroph Project http://neuroph.sourceforge.net
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.events.LearningEvent;
@@ -25,20 +8,30 @@ import org.neuroph.core.events.LearningEventListener;
 import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
 import org.neuroph.core.events.LearningEventType;
-import org.neuroph.core.learning.LearningRule;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.learning.BackPropagation;
-import org.neuroph.nnet.learning.MomentumBackpropagation;
 import org.neuroph.util.TransferFunctionType;
 
 /**
- * This sample shows how to create, train, save and load simple Multi Layer Perceptron for the XOR problem.
- * This sample shows basics of Neuroph API.
- * @author Zoran Sevarac <sevarac@gmail.com>
+ * The program compares a neural network with a extra input node of random data
+ * with a standard neural network on the Sin(x)Cos(2x) problem.
+ *  
+ * @author Michael Glenny <michaelglenny@gmail.com>
+ * @author Laurence Mcfarlane <lmcf013@aucklanduni.ac.nz>
  */
 public class Test implements LearningEventListener {
-
-	BufferedWriter br;
+	//Constant Parameters for the neural network
+	final int NODESINHIDDEN = 3;
+	final int INPUTNODES = 1;
+	final int OUTPUTNODES = 1;
+	final int NUMTESTSPERDATASET = 2;
+	final int NUMDATASETS = 2;
+	final int FILESIZE = 1000;
+	final int TESTINGSIZE = FILESIZE * 10;
+	final int MAXITER = 20000;
+	
+	//Vars used to output formated stats 
+	BufferedWriter bw;
 	boolean firstTime = false;
 	double firstError;
 	int firstIterations;
@@ -47,20 +40,39 @@ public class Test implements LearningEventListener {
 	int tieCounter = 0;
 	double firstTestError;
 	double secondTestError;
-	final int MAXITER = 20000;
+	
+	/**
+	 * Starts the running of the neural network
+	 */
 	public static void main(String[] args) throws IOException {
-		new Test().run(1,"SinCos1000.csv","SinCosRand1000.csv", "TestRunOutput.txt", "Summary.txt");
+		new Test().run("SinCos", "Rand", ".csv", "TestRunOutput.csv", "Summary.csv");
 	}
 
-	public static void createRandomFile(int size){
+	/**
+	 * Creates a file with sin(x)cos(2x) and possibly extra random data between 0 and 2pi
+	 * in format:
+	 * 	x,Rand{-1,1},sin(x)cos(2x)
+	 * or:
+	 *  x,sin(x)cos(2x)
+	 * @param size the number of steps between 0 and 2pi
+	 * @param file the name of the file to write to
+	 */
+	public static void createDataSetFile(int size, String file, boolean random){
 		try {
-			BufferedWriter rw = new BufferedWriter(new FileWriter("SinCosRand"+size+".csv"));
+			BufferedWriter rw = new BufferedWriter(new FileWriter(file));
 
 			double x = 0;
 			double step = (2*Math.PI)/size;
-			for(int i = 0 ; i < size; i++){
-				rw.write(x+","+(Math.random()*2-1)+","+(Math.sin(x)*Math.cos(2*x))+"\n");
-				x += step;
+			if(random){
+				for(int i = 0 ; i < size; i++){
+					rw.write(x+","+(Math.random()*2-1)+","+(Math.sin(x)*Math.cos(2*x))+"\n");
+					x += step;
+				}
+			}else{
+				for(int i = 0 ; i < size; i++){
+					rw.write(x+","+(Math.sin(x)*Math.cos(2*x))+"\n");
+					x += step;
+				}
 			}
 			rw.close();
 
@@ -71,78 +83,107 @@ public class Test implements LearningEventListener {
 	}
 
 	/**
-	 * Runs this sample
+	 * Runs the neural net experiment multiple times on random data sets and initial weights
+	 * and then outputs the summary stats
+	 * All data files names of format (prefix)(randfix)?(NumberIter)(postfix)
 	 * @throws IOException 
 	 */
-	public void run(int inputNodes, String filename, String randomFileName, String outputFile, String outputSumFile) throws IOException {
-		final int NODESINHIDDEN = 3;
-		final int INPUTNODES = 1;
-		final int OUTPUTNODES = 1;
-		final int NUMTESTSPERDATASET = 10;
-		final int NUMDATASETS = 2;
-		final int FILESIZE = 1000;
-		br = new BufferedWriter(new FileWriter(outputFile));
+	public void run(String prefix, String randomfix, String postfix, String outputFile, String outputSumFile) throws IOException {
+		
+		//Create file streams to stats to
+		bw = new BufferedWriter(new FileWriter(outputFile));
 		BufferedWriter brsum = new BufferedWriter(new FileWriter(outputSumFile));
 		
-		createRandomFile(FILESIZE*10);
-		DataSet testSetRand = DataSet.createFromFile("SinCosRand10000.csv",2,1, ",",false);
-		DataSet testSetNonRand = DataSet.createFromFile("SinCos10000.csv", 1, 1, ",", false);
+		//Create the training data set
+		createDataSetFile(FILESIZE, prefix + FILESIZE + postfix, false);
+		
+		//Creating the verification files
+		createDataSetFile(TESTINGSIZE, prefix + randomfix + TESTINGSIZE + postfix, true);
+		DataSet testSetRand = DataSet.createFromFile(prefix + randomfix + TESTINGSIZE + postfix,2,1, ",",false);
+		createDataSetFile(TESTINGSIZE, prefix + TESTINGSIZE + postfix, false);
+		DataSet testSetNonRand = DataSet.createFromFile(prefix + TESTINGSIZE + postfix, 1, 1, ",", false);
+		
+		//Learn on data sets with different random data in them
 		for(int k = 0; k < NUMDATASETS; k++){
-			createRandomFile(FILESIZE);
+			
+			//Create the random dataset
+			createDataSetFile(FILESIZE, prefix + randomfix + FILESIZE + postfix, true);
+			
+			//Set initial stats
+			randomCounter = nonRandomCounter = tieCounter = 0;
+			
+			//Create the training sets used to train the data
+			DataSet trainingSet = DataSet.createFromFile(prefix + FILESIZE + postfix, 1,1, ",", false);
+			DataSet randomTrainingSet = DataSet.createFromFile(prefix + randomfix + FILESIZE + postfix, 2,1,",",false);
+
+			//Learn on the same data set with different initial weights
 			for(int j = 0; j < NUMTESTSPERDATASET; j++){
-				// create training set (logical XOR function)
-				DataSet trainingSet = DataSet.createFromFile(filename, 1,1, ",", false);
-				DataSet randomTrainingSet = DataSet.createFromFile(randomFileName, 2,1,",",false);
-				// create multi layer perceptron
-				MultiLayerPerceptron myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, INPUTNODES, NODESINHIDDEN, OUTPUTNODES);
+				System.out.println("Running for the (" + k + ", " + j + ") time...");
+				
+				MultiLayerPerceptron myMlPerceptron;
+				
+				//Create non-random network
+				myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, INPUTNODES, NODESINHIDDEN, OUTPUTNODES);
 				BackPropagation learningRule = createLearningRule();
 				myMlPerceptron.setLearningRule(learningRule);
 				myMlPerceptron.randomizeWeights();
 				Double[] initialWeights = myMlPerceptron.getWeights();
 
-				// learn the training set
-				System.out.println("Training neural network for " + j + " time...");
+				//Learn on non-random network
 				myMlPerceptron.learn(trainingSet);
 				firstTestError = Test.testNeuralNetwork(myMlPerceptron, testSetNonRand);
-
+				
+				//Create random network
 				myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, INPUTNODES+1, NODESINHIDDEN, OUTPUTNODES);
 				myMlPerceptron.setLearningRule(learningRule);
 				myMlPerceptron.randomizeWeights();
 				Double[] randomWeights = myMlPerceptron.getWeights();
 				
+				//Set the weights to the same as in the non-random case
 				int newWeightLength = initialWeights.length+NODESINHIDDEN;
 				double[] stupidArray = new double[newWeightLength];
 				for(int i = 0; i < newWeightLength; i++){
 					if(i < NODESINHIDDEN){
 						stupidArray[i] = initialWeights[i];
-					}
-					else if(i < 2*NODESINHIDDEN){
+					}else if(i < 2*NODESINHIDDEN){
 						stupidArray[i] = randomWeights[i];
 					}else{ 
 						stupidArray[i] = initialWeights[i-NODESINHIDDEN];
 					}
-
 				}
 				myMlPerceptron.setWeights(stupidArray);
+				
+				//Run random network
 				myMlPerceptron.learn(randomTrainingSet);
 				secondTestError = Test.testNeuralNetwork(myMlPerceptron, testSetRand);
-				br.write(firstTestError + "," + secondTestError + "\n");
+				
+				//Write the errors to the stats file
+				bw.write(firstTestError + "," + secondTestError + "\n");
 			}
-			System.out.println("Test finished:" + nonRandomCounter + " instances of non-random, "
-					+ randomCounter + " instances of randomCounter");
-			System.out.println("There are " + tieCounter + " ties");
-			System.out.println("Number of tests: " + NUMTESTSPERDATASET);
+			
+			//Print the results of the data set
+			System.out.print("Test finished: ");
+			System.out.println("Non-Random: "+ nonRandomCounter+", Random: " + randomCounter + ", Ties: " + tieCounter);
+			
+			//Write the summary data to summary files
 			brsum.write(nonRandomCounter + "," + randomCounter +"," + tieCounter+"\n");
-			randomCounter = nonRandomCounter = tieCounter = 0;
-			br.write("---------------------------------------------\n");
+			bw.write("---------------------------------------------\n");
+			
+			//Flush files
+			brsum.flush();
+			bw.flush();
 		}
-		brsum.flush();
+		
+		//Close writing files
 		brsum.close();
-		br.flush();
-		br.close();
-
+		bw.close();
 
 	}
+	
+	/**
+	 * Creates the standard learning rule for backproagation
+	 * @return BackPropagation
+	 */
 	public BackPropagation createLearningRule(){
 		BackPropagation learningRule = new BackPropagation();
 		learningRule.addListener(this);
@@ -152,10 +193,11 @@ public class Test implements LearningEventListener {
 		learningRule.setBatchMode(false);
 		return learningRule;
 	}
+	
 	/**
-	 * Prints network output for the each element from the specified training set.
+	 * Returns the cumulative error for a neural net over a data set
 	 * @param neuralNet neural network
-	 * @param trainingSet training set
+	 * @param trainingSet test set
 	 */
 	public static double testNeuralNetwork(NeuralNetwork neuralNet, DataSet testSet) {
 		double error = 0;
@@ -165,19 +207,19 @@ public class Test implements LearningEventListener {
 			double[] networkOutput = neuralNet.getOutput();
 			double[] desiredOutput = testSetRow.getDesiredOutput();
 
-			//System.out.print("Input: " + Arrays.toString( testSetRow.getInput() ) );
-			//System.out.println(" Output: " + Arrays.toString( networkOutput) );
-
 			for(int i = 0; i < networkOutput.length; i++){
-				//System.out.println("Error: " + Math.abs(networkOutput[i] - desiredOutput[i]));
 				error += Math.abs(networkOutput[i] - desiredOutput[i]);
 			}
 			
 		}
-		System.out.println("Error for this case: " + error);
 		return error;
 	}
 
+	/**
+	 * Handles the learning event each time the neural net learns
+	 * Will ignore all events except stop, in which case it stores stats
+	 * @param LearningEvent the triggering learning event
+	 */
 	@Override
 	public void handleLearningEvent(LearningEvent event) {
 		BackPropagation bp = (BackPropagation)event.getSource();
@@ -189,20 +231,20 @@ public class Test implements LearningEventListener {
 			}else{
 				firstTime = false;
 				try{
+					//Determine who wins and output the stats to file
 					if(firstIterations < MAXITER && bp.getCurrentIteration() == MAXITER){
-						br.write("non-random," );
+						bw.write("non-random," );
 						nonRandomCounter++;
 					}else if(firstIterations == MAXITER && bp.getCurrentIteration() < MAXITER){
-						br.write("random," );
+						bw.write("random," );
 						randomCounter++;
 					}else{
-						br.write("tie,");
+						bw.write("tie,");
 						tieCounter++;
 					}
-					br.write(firstIterations + "," +firstError + "," + bp.getCurrentIteration() + "," + bp.getTotalNetworkError()+",");
+					bw.write(firstIterations + "," +firstError + "," + bp.getCurrentIteration() + "," + bp.getTotalNetworkError()+",");
 				}
 				catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
